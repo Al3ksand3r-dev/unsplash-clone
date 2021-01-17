f0e09a65cc9e3d6dbc867077d4f41910707179fb
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2021-01-17 21:07:54 +0100

    init commit

---
+++ src/views/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <div class="container__content">
+      <h1>Hello, world!</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `
+            @import "@/styles/variables/_variables.scss";
+            @import "@/styles/mixins/_mixins.scss";
+          `,
+      },
+    },
+  },
+};
